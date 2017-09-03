2fb30899c67958809e08501718e0f2f1b6b27a7d
Andrew Dzygar <dzigar2012@gmail.com>
2017-09-03 15:57:57 +0300

    create ChartValueBean.java

---
+++ ChartValueBean.java
@@ -0,0 +1,35 @@
+package de.bitfork.jba.bean;
+
+import org.apache.commons.lang3.math.NumberUtils;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ChartValueBean implements Comparable<ChartValueBean>{
+
+	@JsonProperty("name")
+	private String name;
+		
+	@JsonProperty("color")
+	private String color;
+	
+	@JsonProperty("data")
+	private double[] data;
+	
+	@Override
+	public int compareTo(ChartValueBean o) {
+		Integer thisName = NumberUtils.toInt(this.getName(), 0);
+		Integer compareName = NumberUtils.toInt(o.getName(), 0);
+		if (thisName == 0 || compareName == 0) {
+			return 0;
+		}
+		return thisName < compareName ? -1 : 1;
+	}
+
+}
